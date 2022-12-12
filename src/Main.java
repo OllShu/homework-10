7f35b2f1bcbe1258f2e8fe8d1422625a7795f00f
dns <shulgaol@mail.ru>
2022-12-12 16:18:43 +0200

    домашка по уроку строки

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String firstName = "Иван";
+        String middleName = "Иванович";
+        String lastName = "Иванов";
+        String fullName = lastName+' '+firstName+' '+middleName;
+        System.out.println(fullName);
+    }
+}
