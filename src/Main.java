928e325d6ba14e7a9cee87b9da6976f9afca5b6e
dns <shulgaol@mail.ru>
2022-12-12 16:28:34 +0200

    2-ая задача

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,14 @@ public class Main {
         String lastName = "Иванов";
         String fullName = lastName+' '+firstName+' '+middleName;
         System.out.println(fullName);
+
+        System.out.println("____________________");
+
+        System.out.println("Вторая задача");
+
+        String fullName2 = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — "+fullName2);
+
+        System.out.println("____________________");
     }
 }
